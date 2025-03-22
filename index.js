f5ad2edf8b8bf934bb89b8c5891546de30e82334
quangdoanh04072004 <quangdoanh04072004@gmail.com>
2025-03-22 16:09:05 +0700

    Tich hop Pug

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
+
+// Thiết lập views
+app.set('views', path.join(__dirname, "views"));
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('clients/pages/home.pug', {
+        pageTitle: "Doanh dep zai so 1"
+    })
+})
+app.get('/tours', (req, res) => {
+    res.render('clients/pages/tour-list', {
+        pageTitle: "Doanh dep zai"
+    })
 })
 
 app.listen(port, () => {
