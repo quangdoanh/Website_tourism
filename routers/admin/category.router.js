c8016f3fabc51306452375f8c4b114785b51930e
quangdoanh04072004 <quangdoanh04072004@gmail.com>
2025-05-10 14:38:09 +0700

    Thêm danh mục

--- routers/admin/category.router.js
+++ routers/admin/category.router.js
@@ -1,9 +1,13 @@
 const route = require("express").Router()
 
+const multer = require('multer');
+const upload = multer();
+
 //Controllers
 const categoryController = require("../../controllers/admin/category.controller")
 // end Controllers
 route.get('/list', categoryController.list);
 route.get('/create', categoryController.create);
+route.post('/create', upload.single('avatar'), categoryController.createPost);
 
 module.exports = route;
